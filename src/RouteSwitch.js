d884fc0249fe5bf3ba490a1b3cea0d2f4d2eb2ff
mckgar <kmgarner23@outlook.com>
2022-11-23 14:11:09 -0700

    Added route for item page

--- src/RouteSwitch.js
+++ src/RouteSwitch.js
@@ -3,16 +3,22 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App';
 import Cart from './components/Cart';
 import Home from './components/Home';
+import ItemInfo from './components/ItemInfo';
 
 const RouteSwitch = () => {
   const [cart, setCart] = useState([]);
+
+  const addToCart = itemid => {
+    setCart([...cart, itemid]);
+  }
 
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<App count={cart.length} />}>
           <Route index element={<Home />} />
-          <Route path='/cart' element={<Cart />} />
+          <Route path=':itemid' element={<ItemInfo addToCart={addToCart} />} />
+          <Route path='/cart' element={<Cart cart={cart} />} />
         </Route>
       </Routes>
     </BrowserRouter>
