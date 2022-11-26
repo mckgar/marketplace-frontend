985bf4c9a59ed1a5d41cd3529fc41c0fad8925c1
mckgar <kmgarner23@outlook.com>
2022-11-26 16:59:31 -0700

    Logo links to home

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,13 @@
 import Navbar from "./Navbar";
 import './Header.css';
+import { Link } from "react-router-dom";
 
 const Header = props => {
   return (
     <header>
-      <h1>Marketplace</h1>
+      <Link to='/' className="title">
+        <h1>Marketplace</h1>
+      </Link>
       <Navbar count={props.count} />
     </header>
   )
