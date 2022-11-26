87bfe60564ad27276b0a4bcba03d108aa47fe5af
mckgar <kmgarner23@outlook.com>
2022-11-26 14:40:25 -0700

    Added home page layout

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,24 @@
+import { useEffect, useState } from "react";
+import { fetchHomePageItems } from "../server-calls";
+import ViewItems from "./ViewItems";
+
 const Home = () => {
+
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    const getItems = async () => {
+      const res = await fetchHomePageItems();
+      setItems(res);
+    }
+    getItems();
+  }, []);
+
   return (
-    <h2>Under Construction</h2>
+    <main>
+      <h2>Check out these items</h2>
+      <ViewItems items={items} />
+    </main>
   );
 }
 
