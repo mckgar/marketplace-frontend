7d6c5da0b64aabb8ba1d6c91cb29b6b8d7ce6134
mckgar <kmgarner23@outlook.com>
2022-11-29 13:14:24 -0700

    Adjusted layout

--- src/components/ItemInfo.js
+++ src/components/ItemInfo.js
@@ -12,7 +12,16 @@ const ItemInfo = () => {
 
   const addToCart = (itemid, quantity) => {
     if (quantity > 0 && quantity <= item.quantity) {
-      setCart([...cart, {itemid, quantity}]);
+      setCart([...cart, { itemid, quantity }]);
+    }
+  }
+
+  const changeQuantity = type => {
+    if (type === '+' && quantity < item.quantity) {
+      setQuantity(quantity + 1);
+    }
+    if (type === '-' && quantity > 1) {
+      setQuantity(quantity - 1);
     }
   }
 
@@ -29,17 +38,18 @@ const ItemInfo = () => {
       <img img={item.image} alt={item.name} />
       <div className='item-view'>
         <div className="item-name">{item.name}</div>
+        <div className="item-seller">{item.seller}</div>
         <div className="item-description">{item.description}</div>
         <div className='item-price'>${item.price}</div>
-        <div className='item-quantity'>Available: {item.quantity}</div>
-        <div className="item-seller">{item.seller}</div>
         <div className="item-category">{item.category}</div>
-        <div className="item-date">{(new Date(item.date_added)).toLocaleString()}</div>
-      </div>
-      <div className="cart-options">
+        <div className='item-quantity'>Available: {item.quantity}</div>
         <div className="add-cart">
-          <input type='number' min={1} max={item.quantity} placeholder={1} value={quantity} onChange={e => setQuantity(e.target.value)} />
-          <button onClick={() => addToCart(params.itemid, quantity)}>Add to Cart</button>
+          <div className="decrease-quantity" onClick={() => changeQuantity('-')}>-</div>
+          <div className="quantity">{quantity}</div>
+          <div className="increase-quantity" onClick={() => changeQuantity('+')}>+</div>
+          <div className="add-button" onClick={() => addToCart(params.itemid, quantity)}>
+            Add to Cart
+          </div>
         </div>
       </div>
     </div>
