570695e49039565e366d6b90f03d00ee327f2e46
mckgar <kmgarner23@outlook.com>
2022-12-22 15:09:56 -0700

    Added filter options

--- src/components/Home.js
+++ src/components/Home.js
@@ -29,18 +29,59 @@ const Home = () => {
     dispatch(getItems([price, category, offset, limit]));
   }, [price, category, offset, limit, dispatch]);
 
+  const filters = (
+    <div className="filters">
+      <select id="sort" onChange={e=> setPrice(e.target.value)} value={price}>
+        <option value='relevent'>Relevent</option>
+        <option value='low'>Price: Low to High</option>
+        <option value='high'>Price: High to Low</option>
+      </select>
+      <select id='category' onChange={e => setCategory(e.target.value)} value={category}>
+        <option value='all'>All Categories</option>
+        <option value='accessories'>Accessories</option>
+        <option value='books'>Books</option>
+        <option value='clothing'>Clothing</option>
+        <option value='decorations'>Decorations</option>
+        <option value='games'>Games</option>
+        <option value='office'>Office</option>
+        <option value='toys'>Toys</option>
+      </select>
+      <select id='limit' onChange={e => setLimit(e.target.value)} value={limit}>
+        <option value={10}>10</option>
+        <option value={20}>20</option>
+        <option value={50}>50</option>
+        <option value={100}>100</option>
+      </select>
+    </div>
+  );
+
   if (isLoading) {
-    return <Loader />;
+    return (
+      <main id='home'>
+        <h2>Check out these items</h2>
+        {filters}
+        <Loader />
+      </main>
+    );
   } else if (error) {
     return (
-      <main>{error}</main>
+      <main id='home'>
+        <h2>Check out these items</h2>
+        {filters}
+        {error}
+      </main>
     );
   }
 
   return (
-    <main>
+    <main id='home'>
       <h2>Check out these items</h2>
+      {filters}
       <ViewItems items={items} />
+      <div className="page-btns">
+        {offset > 0 ? <div className="page-btn" onClick={() => setOffset(offset - limit)}>Previous</div> : null}
+        {items.length >= limit ? <div className="page-btn" onClick={() => setOffset(offset + limit)}>Next</div> : null}
+      </div>
     </main>
   );
 }
