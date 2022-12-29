50cd80bf0c70111a2a9dbcd336d04629320616de
mckgar <kmgarner23@outlook.com>
2022-12-28 21:15:52 -0700

    Added footer

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import './styles/Footer.css'
+import { FaGithub } from 'react-icons/fa';
+
+const Footer = () => {
+  return (
+    <footer>
+      <a href='https://github.com/mckgar/marketplace-frontend'>
+        <FaGithub />
+      </a>
+    </footer>
+  );
+}
+
+export default Footer;
